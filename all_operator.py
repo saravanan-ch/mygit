bfbb09ce763f91295737e3c3f957780112738475
shital kumbhar <shitalkumbhar930@gmail.com>
2026-05-11 00:56:24 +0530

    modified all_operator.py file

--- all_operator.py
+++ all_operator.py
@@ -28,7 +28,7 @@ print("a is less than or equal to b   :",a<=b)    # True--here one condition is 
 print("==" *30)
 print("                         Logical Operator                 "                       )
 print("==" *30)
-#AND operation
+#AND operator
 print("a       b     :  a and b")
 print("_" *60)
 print(f"0       0     :  {0 and 0}         # False")
@@ -37,7 +37,7 @@ print(f"1       0     :  {1 and 0}         # False")
 print(f"1       1     :  {1 and 1}         # True")
 print("_" *60)
 
-#OR operation
+#OR operator
 print("a       b     :  a or b")
 print("_" *60)
 print(f"0       0     :  {0 or 0}         # False")
@@ -45,3 +45,37 @@ print(f"0       1     :  {0 or 1}         # True")
 print(f"1       0     :  {1 or 0}         # True")
 print(f"1       1     :  {1 or 1}         # True")
 print("_" *60)
+
+#NOT operator
+print("a      :  not a ")
+print("_" *60)
+print(f"0      : {not 0}")
+print(f"1      : {not 1}\n\n")
+
+
+print("==" *30)
+print("                        Membership operator                 "                       )
+print("==" *30)
+num_list = [1,2,3,4,5]
+print (f"checking 3 is in list     :{3 in num_list}")
+print (f"checking 5 is in list     :{5 in num_list}")
+print (f"checking 6 is in list     :{6 in num_list}")
+print (f"checking 6 is not in list :{6  not in num_list}")
+print (f"checking 3 is not in list :{3 not in num_list}")
+
+
+
+print("==" *30)
+print("                        Identity operator                 "                       )
+print("==" *30)
+a = [1,2,3]
+a = b
+print("a = [1,2,3]\n a = b")
+print(f"a is b     : {a is b}               #both a and b points same list object")
+print(f"a is  not b: {a is not b}\n")     
+
+a = [1,2,3]
+b = [1,2,3]
+print(f"a = {a}\nb = {b}")
+print(f"a is  b    :{a is b}         #even though values are same they stored in different memory location")
+print(f"a is not b :{a is not b}          #both have diffwrant memory location\n")       
